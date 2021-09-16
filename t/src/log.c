de597dcc666b42a093b5767f1c84e296135a59aa
Jim Garlick <garlick.jim@gmail.com>
2021-09-16 14:30:06 -0700

    testsuite: add standalone pmix test programs
    Problem: we need a way to exercise the simplest functionality
    of a pmix shell plugin before hitting it with openpmi.
    
    First create a 'libtestutil' internal library for these tests
    and pull in monotime and log from flux-core libutil.
    
    Add barrier test which just times an empty PMIx_Fence().
    
    Add bizcard test which does a simple business card exchange.

---
+++ t/src/log.c
@@ -0,0 +1,158 @@
+/************************************************************\
+ * Copyright 2014 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#if HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <limits.h>
+#include <ctype.h>
+#include <libgen.h>
+#include <syslog.h>
+#include <errno.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+
+#include "log.h"
+
+extern char *__progname;
+static char *prog = NULL;
+
+void
+log_init (char *p)
+{
+    if (!p)
+        prog = __progname;
+    else
+        prog = basename (p);
+}
+
+void
+log_fini (void)
+{
+}
+
+static void
+_verr (int errnum, const char *fmt, va_list ap)
+{
+    char *msg = NULL;
+    char buf[128];
+    const char *s = strerror (errnum);
+
+    if (!prog)
+        log_init (NULL);
+    if (vasprintf (&msg, fmt, ap) < 0) {
+        (void)vsnprintf (buf, sizeof (buf), fmt, ap);
+        msg = buf;
+    }
+    fprintf (stderr, "%s: %s: %s\n", prog, msg, s);
+    if (msg != buf)
+        free (msg);
+}
+
+static void
+_vlog (const char *fmt, va_list ap)
+{
+    char *msg = NULL;
+    char buf[128];
+
+    if (!prog)
+        log_init (NULL);
+    if (vasprintf (&msg, fmt, ap) < 0) {
+        (void)vsnprintf (buf, sizeof (buf), fmt, ap);
+        msg = buf;
+    }
+    fprintf (stderr, "%s: %s\n", prog, msg);
+    if (msg != buf)
+        free (msg);
+}
+
+/* Log message and errno string, then exit.
+ */
+void
+log_err_exit (const char *fmt, ...)
+{
+    va_list ap;
+
+    va_start (ap, fmt);
+    _verr (errno, fmt, ap);
+    va_end (ap);
+    exit (1);
+}
+
+/* Log message and errno string.
+ */
+void
+log_err (const char *fmt, ...)
+{
+    va_list ap;
+
+    va_start (ap, fmt);
+    _verr (errno, fmt, ap);
+    va_end (ap);
+}
+
+
+/* Log message and errnum string, then exit.
+ */
+void
+log_errn_exit (int errnum, const char *fmt, ...)
+{
+    va_list ap;
+
+    va_start (ap, fmt);
+    _verr (errnum, fmt, ap);
+    va_end (ap);
+    exit (1);
+}
+
+/* Log message and errnum string.
+ */
+void
+log_errn (int errnum, const char *fmt, ...)
+{
+    va_list ap;
+
+    va_start (ap, fmt);
+    _verr (errnum, fmt, ap);
+    va_end (ap);
+}
+
+/* Log message, then exit.
+ */
+void
+log_msg_exit (const char *fmt, ...)
+{
+    va_list ap;
+
+    va_start (ap, fmt);
+    _vlog (fmt, ap);
+    va_end (ap);
+    exit (1);
+}
+
+/* Log message.
+ */
+void
+log_msg (const char *fmt, ...)
+{
+    va_list ap;
+
+    va_start (ap, fmt);
+    _vlog (fmt, ap);
+    va_end (ap);
+}
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
---
+++ t/src/log.h
@@ -0,0 +1,39 @@
+/************************************************************\
+ * Copyright 2014 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, COPYING)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\************************************************************/
+
+#ifndef _UTIL_LOG_H
+#define _UTIL_LOG_H
+
+#include <errno.h>
+#include <stdarg.h>
+#include <stdlib.h>
+
+void log_init (char *cmd_name);
+void log_fini (void);
+
+void log_err_exit (const char *fmt, ...)
+        __attribute__ ((format (printf, 1, 2), noreturn));
+void log_err (const char *fmt, ...)
+        __attribute__ ((format (printf, 1, 2)));
+void log_errn_exit (int errnum, const char *fmt, ...)
+        __attribute__ ((format (printf, 2, 3), noreturn));
+void log_errn (int errnum, const char *fmt, ...)
+        __attribute__ ((format (printf, 2, 3)));
+void log_msg_exit (const char *fmt, ...)
+        __attribute__ ((format (printf, 1, 2), noreturn));
+void log_msg (const char *fmt, ...)
+        __attribute__ ((format (printf, 1, 2)));
+
+
+#endif /* !_UTIL_LOG_H */
+
+/*
+ * vi:tabstop=4 shiftwidth=4 expandtab
+ */
